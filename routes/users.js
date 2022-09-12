445c6286a7d4253d5e00a9b3a147c68783c57be8
Daniel Gonzalez <danielgnzch@gmail.com>
2022-09-12 12:44:22 +0200

    Closed #24 Add routes to get data from User model

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const { isAuthenticated, isOwner } = require("../middlewares/jwt");
+const ErrorResponse = require("../utils/error");
 const fileUploader = require("../config/cloudinary.config");
 
 // @desc    Edit a user
@@ -60,5 +61,31 @@ router.put(
     }
   }
 );
+
+// @desc    Get single user
+// @route   GET /api/v1/users/:id
+// @access  Private
+router.get("/:id", isAuthenticated, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return next(new ErrorResponse(`User not found by id: ${id}`, 404));
+    }
+    const publicUser = {
+      email: user.email,
+      fullName: user.fullName,
+      profileImage: user.profileImage,
+      profession: user.profession,
+      location: user.location,
+      headline: user.headline,
+      about: user.about,
+      contactInfo: user.contactInfo,
+    }
+    res.status(200).json({ data: publicUser });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
